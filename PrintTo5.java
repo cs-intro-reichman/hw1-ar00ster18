ca6ca18dd0a74c4f5765141f87da19a92397796c
ar00ster18 <arielarusy12@gmail.com>
2024-11-09 10:21:27 +0200

    Just a test

--- PrintTo5.java
+++ PrintTo5.java
@@ -4,6 +4,8 @@ public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        int j = 1;
         // Complete the program's code here:
+        System.out.println(i+j);
     }
  }
