651a545a65c48c3cb5d404caca70a0b47151c32d
ar00ster18 <arielarusy12@gmail.com>
2024-11-09 10:36:49 +0200

    Completed the code

--- PrintTo5.java
+++ PrintTo5.java
@@ -2,10 +2,13 @@
 // (Same program as shown in lecture 1-1).
 public class PrintTo5 {
 	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
+		// Declares an int variable, and sets it to 0
         int i = 0;
-        int j = 1;
         // Complete the program's code here:
-        System.out.println(i+j);
+        while (i < 6){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Done");
     }
  }
