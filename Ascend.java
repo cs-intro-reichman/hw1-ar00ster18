9fa874f9f9e8bd65b236f284f9ac3cdeea456f90
ar00ster18 <arielarusy12@gmail.com>
2024-11-10 16:58:01 +0200

    made some more chnages

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,10 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int)((Math.random())*lim);
+		int b = (int)((Math.random())*lim);
+		int c = (int)((Math.random())*lim);
+
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,11 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		int hund = num/100;
+		int tens = (num/10)%10;
+		int ones = (num%100)%10;
+
+		System.out.println(hund + " hundreds, " + tens + " tens, " + ones + " ones.");
 	}
 }
